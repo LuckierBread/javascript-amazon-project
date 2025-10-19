1bfab3053b8f4250970c0a13f9aaecc9edd480f6
LuckierBread <yoh.asuchra@hotmail.com>
2025-10-19 11:38:02 -0600

    added defalt cart for dev purposes.

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export let cart = getFromStorage()||[]
+export let cart = getFromStorage()||[
+    {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOption:1
+    },{
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId:0
+    }]
 
 export function addToCart(productId) {
     let itemInCart = false
@@ -13,15 +22,24 @@ export function addToCart(productId) {
     if(!itemInCart){
         cart.push({
             id: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId:0
         })
     }
     saveToStorage()
 }
 
 export function removeFromCart(productId){
-    cart = cart.filter((item)=> item.id!==productId)
-    saveToStorage()
+    cart = cart.filter((item)=> item.id!==productId);
+    saveToStorage();
+}
+
+export function getCartQuantity(){
+    let cartCount = 0;
+    cart.forEach(item=>{
+        cartCount += item.quantity;
+    });
+    return cartCount;
 }
 
 function saveToStorage(){
