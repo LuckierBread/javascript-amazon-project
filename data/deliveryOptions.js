7402543d3d8fd1b0702e275fe64146876ed2df31
LuckierBread <yoh.asuchra@hotmail.com>
2025-10-19 11:38:48 -0600

    updated delivery options to include free items

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+    id:0,
+    priceCents:0,
+    days:1
+    },{
+    id: 1,
+    priceCents:499,
+    days: 3
+    },{
+    id:2,
+    priceCents:999,
+    days:7
+}]
