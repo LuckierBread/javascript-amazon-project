a4b01eeccb23d16ba616e7e52fed55e266d8a780
LuckierBread <yoh.asuchra@hotmail.com>
2025-10-22 19:00:03 -0600

    added loadFromStorage, also muted inital test cart

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,20 @@
-export let cart = getFromStorage()||[
-    {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId:1
-    },{
-        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId:0
-    }]
+export let cart 
+
+loadFromStorage()
+
+export function loadFromStorage(){
+cart = JSON.parse(localStorage.getItem('cart'))||[
+    // {
+    //     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    //     quantity: 2,
+    //     deliveryOptionId:1
+    // },{
+    //     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    //     quantity: 1,
+    //     deliveryOptionId:0
+    // }
+]
+}
 
 export function addToCart(productId) {
     let itemInCart = false
@@ -60,9 +67,6 @@ export function updateDeliveryOption(productId,newDeliveryOptionId){
     })
 }
 
-function saveToStorage(){
+export function saveToStorage(){
     localStorage.setItem('cart',JSON.stringify(cart))
-}
-function getFromStorage(){
-    return JSON.parse(localStorage.getItem('cart'))
 }
